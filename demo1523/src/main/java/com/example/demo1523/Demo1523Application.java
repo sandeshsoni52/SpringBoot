b9bcb12a9dcea440286d93cd2bf9a534ea787ea3
unknown <learnersolution2024@gmail.com>
2025-02-13 15:53:06 +0530

    Working sb java21

---
+++ demo1523/src/main/java/com/example/demo1523/Demo1523Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1523;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1523Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1523Application.class, args);
+	}
+
+}
