41f70a20ba24b39df244e8d88665b85dd8028dbd
unknown <learnersolution2024@gmail.com>
2025-02-14 11:40:20 +0530

    okk 1140

---
+++ demo1104/demo1104/src/main/java/com/example/demo1104/Demo1104Application.java
@@ -0,0 +1,13 @@
+package com.example.demo1104;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo1104Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo1104Application.class, args);
+	}
+
+}
