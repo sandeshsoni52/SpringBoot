a7800b94eb39c47abce8c0c5217be39410448e85
unknown <learnersolution2024@gmail.com>
2025-02-14 12:08:23 +0530

    okk1207 till PACKAGE_repo

---
+++ demo1104/demo1104/src/main/java/com/example/demo1104/repo/PersonRepo.java
@@ -0,0 +1,9 @@
+package com.example.demo1104.repo;
+
+import com.example.demo1104.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface PersonRepo extends JpaRepository<Person, Long> {
+}
