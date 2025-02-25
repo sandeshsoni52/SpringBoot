d662ddd2e6235327974633d24bc4ba0535abad76
unknown <learnersolution2024@gmail.com>
2025-02-25 12:30:26 +0530

    issue in getcategories 1230

--- t1147/src/main/java/com/company1/project1/t1147/model/CourseData.java
+++ t1147/src/main/java/com/company1/project1/t1147/model/CourseData.java
@@ -8,4 +8,9 @@ import java.util.Map;
 @Data
 public class CourseData {
     private Map<String, Map<String, Map<String, List<String>>>> categories;
+
+    public static void main(String[] args) {
+        CourseData data = new CourseData();
+        System.out.println(data.getCategories());  // This should not show an error
+    }
 }
