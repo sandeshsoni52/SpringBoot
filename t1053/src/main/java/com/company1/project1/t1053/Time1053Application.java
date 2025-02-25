1f793b69974b790c3a545a66a96eed9b69f85c6f
unknown <learnersolution2024@gmail.com>
2025-02-25 11:35:20 +0530

    okk1135 cli input

--- t1053/src/main/java/com/company1/project1/t1053/Time1053Application.java
+++ t1053/src/main/java/com/company1/project1/t1053/Time1053Application.java
@@ -1,14 +1,25 @@
 package com.company1.project1.t1053;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
-public class Time1053Application {
+public class Time1053Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Time1053Application.class, args);
-		System.out.println("hhello1103");
+		System.out.println("hhello1129");
+	}
+	@Override
+	public void run(String... args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter your name: ");
+		String name = scanner.nextLine(); // Read user input
+		System.out.println("Hello, " + name + "! Welcome to Spring Boot."); // Print greeting
+		scanner.close(); // Close scanner
 	}
 
 }
