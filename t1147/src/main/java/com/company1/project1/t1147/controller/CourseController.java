b4ea3db5b9ff028971af85e62210cf5b123299e0
unknown <learnersolution2024@gmail.com>
2025-02-26 11:10:28 +0530

    okk1110

--- t1147/src/main/java/com/company1/project1/t1147/controller/CourseController.java
+++ t1147/src/main/java/com/company1/project1/t1147/controller/CourseController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/courses")
@@ -18,7 +17,7 @@ public class CourseController {
     }
 
     /**
-     * Get all available categories.
+     * Get all available levels.
      */
     @GetMapping("/levels")
     public List<String> getCategories() {
@@ -26,7 +25,7 @@ public class CourseController {
     }
 
     /**
-     * Get institutes based on selected category.
+     * Get faculties based on selected level.
      */
     @GetMapping("/faculties/{category}")
     public List<String> getFaculties(@PathVariable String category) {
@@ -34,12 +33,22 @@ public class CourseController {
     }
 
     /**
-     * Get courses based on category and institute.
+     * Get degrees based on level and faculty.
      */
     @GetMapping("/courses/{category}/{faculty}")
     public List<String> getCourses(@PathVariable String category, @PathVariable String faculty) {
         return courseService.getCourses().getCategories().get(category).get(faculty).values().stream()
                 .flatMap(List::stream)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Get institute based on level and faculty.
+     */
+    @GetMapping("/courses/{category}/{faculty}/{institute}")
+    public List<String> getCourses(@PathVariable String category, @PathVariable String faculty,
+            @PathVariable String institute) {
+        return courseService.getCourses().getCategories().get(category).get(faculty).get(institute).stream()
                 .collect(Collectors.toList());
     }
 }
