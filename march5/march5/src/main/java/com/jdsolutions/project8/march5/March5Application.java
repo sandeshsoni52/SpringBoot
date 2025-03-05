cb58b21e39914066c6bfedbc1fd7d3d4b997fbd8
unknown <learnersolution2024@gmail.com>
2025-03-05 12:45:45 +0530

    edited properties

---
+++ march5/march5/src/main/java/com/jdsolutions/project8/march5/March5Application.java
@@ -0,0 +1,13 @@
+package com.jdsolutions.project8.march5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class March5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(March5Application.class, args);
+	}
+
+}
